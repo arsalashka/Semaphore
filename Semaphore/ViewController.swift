7a3a80f5c573b1ae9f78d8109be5fc4222aa401e
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-01-05 22:06:09 +0300

    beta version

--- Semaphore/ViewController.swift
+++ Semaphore/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var redLightView: UIView!
+	@IBOutlet var yellowLightView: UIView!
+	@IBOutlet var greenLightView: UIView!
+	@IBOutlet var actionButton: UIButton!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		redLightView.alpha = 0.3
+		yellowLightView.alpha = 0.3
+		greenLightView.alpha = 0.3
+
+		redLightView.layer.cornerRadius = redLightView.frame.size.width / 2
+		yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
+		greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
+		actionButton.layer.cornerRadius = 10
 	}
 
 
-}
+	@IBAction func actionButtonPressed() {
 
+		if actionButton.titleLabel?.text == "START" {
+			actionButton.setTitle("NEXT", for: .normal)
+			redLightView.alpha = 1
+			return
+		}
+
+		if redLightView.alpha == 1 {
+			redLightView.alpha = 0.3
+			yellowLightView.alpha = 1
+			return
+		}
+
+		if yellowLightView.alpha == 1 {
+			yellowLightView.alpha = 0.3
+			greenLightView.alpha = 1
+			return
+		}
+
+		if greenLightView.alpha == 1 {
+			greenLightView.alpha = 0.3
+			redLightView.alpha = 1
+			return
+		}
+	}
+}
