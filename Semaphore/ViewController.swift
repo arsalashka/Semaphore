3c3fcd84f4c3e5b6afd91af01748c511642564bb
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-01-07 02:17:28 +0300

    constraints updated, enums and switch used

--- Semaphore/ViewController.swift
+++ Semaphore/ViewController.swift
@@ -7,41 +7,72 @@
 
 import UIKit
 
+enum CurrentLight {
+	case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
 	@IBOutlet var redLightView: UIView!
 	@IBOutlet var yellowLightView: UIView!
 	@IBOutlet var greenLightView: UIView!
+
 	@IBOutlet var actionButton: UIButton!
+
+	private let lightOn: CGFloat = 1
+	private let lightOff: CGFloat = 0.3
+	private var currentLight = CurrentLight.red
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		redLightView.alpha = 0.3
-		yellowLightView.alpha = 0.3
-		greenLightView.alpha = 0.3
+		redLightView.alpha = lightOff
+		yellowLightView.alpha = lightOff
+		greenLightView.alpha = lightOff
 
-		redLightView.layer.cornerRadius = redLightView.frame.size.width / 2
-		yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
-		greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
 		actionButton.layer.cornerRadius = 10
 	}
 
+	override func viewWillLayoutSubviews() {
+		redLightView.layer.cornerRadius = redLightView.frame.width / 2
+		yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+		greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+	}
 
 	@IBAction func actionButtonPressed() {
 
-		if actionButton.titleLabel?.text == "START" {
+//		if actionButton.currentTitle == "START" {
+//			actionButton.setTitle("NEXT", for: .normal)
+//			redLightView.alpha = lightOn
+//		} else if redLightView.alpha == lightOn {
+//			redLightView.alpha = lightOff
+//			yellowLightView.alpha = lightOn
+//		} else if yellowLightView.alpha == lightOn {
+//			yellowLightView.alpha = lightOff
+//			greenLightView.alpha = lightOn
+//		} else if greenLightView.alpha == lightOn {
+//			greenLightView.alpha = lightOff
+//			redLightView.alpha = lightOn
+//		}
+
+		if actionButton.currentTitle == "START" {
 			actionButton.setTitle("NEXT", for: .normal)
-			redLightView.alpha = 1
-		} else if redLightView.alpha == 1 {
-			redLightView.alpha = 0.3
-			yellowLightView.alpha = 1
-		} else if yellowLightView.alpha == 1 {
-			yellowLightView.alpha = 0.3
-			greenLightView.alpha = 1
-		} else if greenLightView.alpha == 1 {
-			greenLightView.alpha = 0.3
-			redLightView.alpha = 1
+		}
+
+		switch currentLight {
+		case .red:
+			currentLight = .yellow
+			redLightView.alpha = lightOn
+			greenLightView.alpha = lightOff
+		case .yellow:
+			currentLight = .green
+			redLightView.alpha = lightOff
+			yellowLightView.alpha = lightOn
+
+		case .green:
+			currentLight = .red
+			yellowLightView.alpha = lightOff
+			greenLightView.alpha = lightOn
 		}
 	}
 }
